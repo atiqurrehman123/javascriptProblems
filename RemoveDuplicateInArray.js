ef808be778616625af0d66fcf68286e7c4b6ff89
atiq ur Rehman <atiqu555@gmail.com>
2023-04-19 22:13:19 +0500

    Remove-last-changes

---
+++ RemoveDuplicateInArray.js
@@ -0,0 +1,34 @@
+var arr=[1,2,3,4,4,5,3];
+// One method
+// const removedupliate=arr.filter((item,index,array)=>{
+//   console.log(item,index,array.indexOf(item))
+//   return array.indexOf(item)===index
+// })
+// console.log(removedupliate,"removeduplicate")
+
+
+//sort method
+// let b=[];
+// arr.sort();
+// let temp;
+// for(let i=0;i<arr.length;i++){
+//   if(arr[i]!==temp){
+//     b.push(arr[i])
+//     temp=arr[i]
+//   }
+// }
+// console.log(b)
+
+
+//Third method
+// let newarray=[...new Set(arr)];
+// console.log(newarray)
+
+
+//Fourth method using object;
+let obj={};
+for(let i of arr){
+  obj[i]=true;
+}
+let uniqueArray=Object.keys(obj)
+console.log(uniqueArray)
