82a3e02080101f2775342ad2012a4716cb862f49
atiq ur Rehman <atiqu555@gmail.com>
2023-04-20 12:23:46 +0500

    Comments-added-into-LongestString-file

--- LongestString.js
+++ LongestString.js
@@ -1,14 +1,20 @@
 // Finding Longest String in Array
 const arrayOfString=["Pakistan","Iran","Dubai","America"];
 const longestString=(arrayString)=>{
+    // declare variable for saving longestString
     let LongestString=""
+    // loop over all array of string
     for(let i=0;i<arrayOfString.length;i++){
+        // checking if string length is less then 0 of LongestString variable then give first index;
         if(LongestString.length<0){
             LongestString = arrayString[i];
-        }else if(LongestString.length<arrayString[i].length){
+        }
+        // checking length of array coming string with save LongestString length
+        else if(LongestString.length<arrayString[i].length){
             LongestString=arrayString[i]
         }
     }
     console.log(LongestString)
+    return LongestString
 }
 longestString(arrayOfString)
