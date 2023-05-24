136a47f715009fb72fc39bb5b77897c749079457
atiq ur Rehman <atiqu555@gmail.com>
2023-05-24 11:32:50 +0500

    Dimand star pattern added

--- starproblem.js
+++ starproblem.js
@@ -108,15 +108,116 @@
 //   *****
 //  *******
 // *********
-let n = 9;
+// let n = 5;
+// let result = ""
+// for (let i = 1; i <= n; i++) {
+//     // FOR PRINDING SPACES
+//     for (let j = 1; j <= n-i; j++) {
+//     result+=" "
+//     }
+//     for(let k=0;k<2*i-1;k++){
+//         result+="*"
+//     }
+
+//     result += "\n"
+// }
+// console.log(result)
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+// let n = 5;
+// let result = ""
+// for (let i = 1; i <= n; i++) {
+//     // FOR PRINDING SPACES
+//     for (let j = 1; j <i; j++) {
+//         result += " "
+//     }
+//     // For printing stars
+//     for (let k = 0; k <2 * (n-i)-1; k++) {
+//         if(i===1){
+//             result += "*"
+//         }else if(k==1 || k==3){
+//             result += " "
+//         }else{
+//             result+="*"
+//         }
+    
+//     }
+
+//     result += "\n"
+// }
+// console.log(result)
+// //     *
+// //    * *
+// //   *   *
+// //  *     *
+// // *********
+// let n = 5;
+// let result = ""
+// for (let i = 1; i <= n; i++) {
+
+//     // FOR PRINTING SPACES
+//     for(let j=1;j<=n-i;j++){
+//         result+=" "
+//     }
+
+//     //printing stars
+//     for(let k=0;k<2*i-1;k++){
+
+//         if(i===1 || i===n){
+//             result +="*"
+//         }
+//         else
+//         {
+//             if(k===0 || k===2*i-2){
+//                 result +="*"
+//             }else{
+//                 result +=" "
+//             }
+//         }
+//     }
+//     result +="\n"
+    
+// }
+// console.log(result)
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+let n = 5;
 let result = ""
 for (let i = 1; i <= n; i++) {
-    for (let j = 0; j < 3-i-1; j++) {
-       result+=" "
+
+    // FOR PRINTING SPACES
+    for (let j = 1; j <= n - i; j++) {
+        result += " "
     }
-    for(let k=0;k<i;k++){
-        result+="*"
+
+    //printing stars
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        result+="*"        
     }
     result += "\n"
+
+}
+for (let row = 1; row <= n; row++) {
+    // for Printing spaces
+    for(let s=1;s<=row;s++){
+        result +=" "
+    }
+    for (let k = (n - row) * 2 - 1; k > 0; k--) {
+        result += "*";
+    }
+
+    result += "\n"
+
 }
 console.log(result)
